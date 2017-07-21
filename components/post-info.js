506618c023f38fac55c3b03b7184af81d990da73
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-07-21 09:38:19 -0700

    Reorganize

--- components/post-info.js
+++ components/post-info.js
@@ -2,31 +2,36 @@ import PropTypes from 'prop-types'
 import Link from 'next/link'
 
 const PostInfo = ({ id, date, title, highlight, header }) => {
-  const polygon = '30,0 330,0 330,170 190,170 180,180 170,170 30,170'
+  const polygon = '0,0 300,0 300,170 160,170 150,180 140,170 0,170'
 
   return (
-    <Link prefetch href={`/${new Date(date).getFullYear()}/${id}`}>
-      <div
-        className='post flex-child relative h240 w360 round cursor-pointer'
-        onMouseEnter={highlight}
-        onClick={highlight}
-      >
-        <svg height='220' width='360'>
-          <defs>
-            <pattern id={'img' + id} patternUnits='userSpaceOnUse' height='180' width='360'>
-              <image x='0' y='0' height='180' width='360' xlinkHref={header} preserveAspectRatio='xMinYMin slice' />
-            </pattern>
-            <linearGradient id={'grad' + id} x1='0%' y1='100%' x2='100%' y2='0%'>
-              <stop offset='0%' style={{stopColor: '#962b18', stopOpacity: 0.5}} />
-              <stop offset='100%' style={{stopColor: '#e2e041', stopOpacity: 0.2}}/>
-            </linearGradient>
-          </defs>
-          <polygon points={polygon} fill={`url(#${'img' + id})`} />
-          <polygon className='filter' points={polygon} fill={`url(#${'grad' + id})`} />
-          <line x1='0' y1='190' x2='360' y2='190' style={{stroke: '#777d9b', strokeWidth: 2}}/>
-          <circle cx='180' cy='190' r='5' style={{stroke: '#ffffff', strokeWidth: 2, fill: '#c63a22'}}/>
-          <text className='title' x='42' y='150' fill='#ffffff'>{title.toUpperCase()}</text>
-          <text className='date' x='180' y='212' fill='#777d9b' textAnchor='middle' >{date}</text>
+      <div>
+        <Link prefetch href={`/${new Date(date).getFullYear()}/${id}`}>
+          <svg
+            className='post flex-child relative w300 mx30 unround cursor-pointer'
+            onMouseEnter={highlight}
+            onClick={highlight}
+            height='180' width='360'
+          >
+            <defs>
+              <pattern id={'img' + id} patternUnits='userSpaceOnUse' height='180' width='360'>
+                <image x='0' y='0' height='180' width='360' xlinkHref={header} preserveAspectRatio='xMinYMin slice' />
+              </pattern>
+              <linearGradient id={'grad' + id} x1='0%' y1='100%' x2='100%' y2='0%'>
+                <stop offset='0%' style={{stopColor: '#962b18', stopOpacity: 0.5}} />
+                <stop offset='100%' style={{stopColor: '#e2e041', stopOpacity: 0.2}}/>
+              </linearGradient>
+            </defs>
+            <polygon points={polygon} fill={`url(#${'img' + id})`} />
+            <polygon className='filter' points={polygon} fill={`url(#${'grad' + id})`} />
+            <text className='title' x='30' y='150' fill='#ffffff'>{title.toUpperCase()}</text>
+          </svg>
+        </Link>
+
+        <svg className='timeline' height='42' width='360'>
+          <line x1='0' y1='12' x2='360' y2='12' style={{stroke: '#777d9b', strokeWidth: 2}}/>
+          <circle cx='180' cy='12' r='5' style={{stroke: '#ffffff', strokeWidth: 2, fill: '#c63a22'}}/>
+          <text className='date' x='180' y='36' fill='#777d9b' textAnchor='middle' >{date}</text>
         </svg>
 
         <style jsx global>{`
@@ -51,7 +56,6 @@ const PostInfo = ({ id, date, title, highlight, header }) => {
           }
         `}</style>
       </div>
-    </Link>
   )
 }
 
