ef87deb7c6f298fc706b2ce92b48f8fbfbc9577c
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-08-10 23:51:35 -0700

    Do not resize already resized, or smaller, photos

--- helpers/optimize.js
+++ helpers/optimize.js
@@ -6,12 +6,16 @@ function sharpen (file) {
   if (file.indexOf('.optim.') > -1) return
   if (!(file.endsWith('jpg') || file.endsWith('JPG'))) return
 
-  return sharp(file)
-    .resize(2000)
-    .sharpen()
-    .toFile(file + '.optim.jpg')
+  sharp(file)
+    .metadata()
+    .then(info => {
+      if (info.width <= 2000) return Promise.reject(new Error('should not resize smaller photo'))
+      else return Promise.resolve(sharp(file))
+    })
+    .then((s) => s.resize(2000).sharpen().toFile(file + '.optim.jpg'))
     .then(() => fs.rename(file + '.optim.jpg', file))
     .then(() => console.log(file), 'optimized')
+    .catch(console.log)
 }
 
 var imgPath = path.resolve(process.cwd(), process.argv[2])
