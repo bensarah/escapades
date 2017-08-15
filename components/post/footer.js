b416a60395801c4efbcc9b2aa0bcdce4b935af80
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-08-15 10:43:42 -0700

    Fix footer img

--- components/post/footer.js
+++ components/post/footer.js
@@ -2,35 +2,33 @@ import PropTypes from 'prop-types'
 import Emoji from './emoji'
 import palette from '../../styles/palette'
 import Link from 'next/link'
-import Isvg from 'react-inlinesvg'
 
 const Footer = ({ children }) => (
-  <div className='footer'>
+  <div className='footer relative hmin120 py30 px30'>
     <div className='w-full display-block'/>
-      <div className='flex-parent'>
-        <div className='hmin120 py36 px30 flex-child flex-child--grow'>
-          <span className='clickable cursor-pointer' onClick={() => window.scrollTo(window.pageYOffset, 0)}>
-            <Emoji name='arrow-up'/><span className='pl6'>Haut de page</span>
-          </span>
-          <span className='px18'/>
-          <Link href='/'>
-            <a className='cursor-pointer'><Emoji name='house'/><span className='pl6'>Page d’accueil</span></a>
-          </Link>
-          <span className='px18'/>
-          <Link href='/about'>
-            <a className='cursor-pointer'><Emoji name='couple'/><span className='pl6'>À propos</span></a>
-          </Link>
-          <br/>
-          <div className='bottom pt30 color-lighten25'>
-            <span>Copyright © {new Date().getFullYear()} Sarah & Benjamin</span><br/>
-            <span>Made with <Emoji name='heart'/> in San Francisco and Paris</span>
-          </div>
-        </div>
-        <div className='flex-child flex-child--no-shrink mt36'>
-          <Isvg className='pr24' src={`/static/deco/footer-vector.svg`}></Isvg>
+      <div>
+        <span className='clickable cursor-pointer' onClick={() => window.scrollTo(window.pageYOffset, 0)}>
+          <Emoji name='arrow-up'/><span className='pl6'>Haut de page</span>
+        </span>
+        <span className='px18'/>
+
+        <Link href='/'>
+          <a className='cursor-pointer'><Emoji name='house'/><span className='pl6'>Page d’accueil</span></a>
+        </Link>
+        <span className='px18'/>
+
+        <Link href='/about'>
+          <a className='cursor-pointer'><Emoji name='couple'/><span className='pl6'>À propos</span></a>
+        </Link>
+        <br/>
+
+        <div className='bottom pt30 color-lighten25'>
+          <span>Copyright © {new Date().getFullYear()} Sarah & Benjamin</span><br/>
+          <span>Made with <Emoji name='heart'/> in San Francisco and Paris</span>
         </div>
       </div>
 
+      <img className='footer-img absolute bottom right pr36 pt30 h120' src={`/static/deco/footer-vector.svg`}></img>
     <style jsx>{`
       .footer {
         background-color: ${palette.bleuNuit};
@@ -39,6 +37,16 @@ const Footer = ({ children }) => (
 
       .clickable:hover, a:hover {
         color: white;
+      }
+
+      .footer-img {
+        display: none;
+      }
+
+      @media screen and (min-width: 800px) {
+        .footer-img {
+          display: block;
+        }
       }
     `}</style>
   </div>
