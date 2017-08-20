2382c1bf8fc96f7c89d524f65195611acb45ce1b
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-08-20 11:06:16 -0700

    Prefooter was commited

--- components/pre-footer.js
+++ components/pre-footer.js
@@ -1,23 +1,20 @@
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-import Emoji from './emoji'
-import posts from '../posts'
 import palette from '../styles/palette'
 
-const PreFooter = ({except}) => {
-  var post = randomPost(posts, except)
-
+const PreFooter = ({cta, quote, url}) => {
   return (
     <div className='px18 py30 bg-gray-faint color-gray align-center'>
-        <Link prefetch href={post.url}>
+        <Link prefetch href={url}>
             <a className='cursor-pointer'>
-              <Emoji name='smiley'/> Avide d’autres aventures&nbsp;? Lisez notre récit de {post.title}
-              <span className='icon-inliner px6'><svg className='icon icon--s'><use xlinkHref='#icon-arrow-right'/></svg></span>
+              {cta}
             </a>
         </Link>
-        <p className='quote pt6 txt-l'>
-          « La dernière lignée d’arbres laisse apparaître un gigantesque pierrier et un névé. Le sommet est en haut... »
-        </p>
+        {
+          quote
+          ? <p className='quote pt6 txt-l'>«&nbsp;{quote}&nbsp;»</p>
+          : null
+        }
       <style jsx>{`
         .quote {
           color: ${palette.brique};
@@ -36,14 +33,10 @@ const PreFooter = ({except}) => {
   )
 }
 
-function randomPost (posts, except) {
-  var filtered = posts.filter(p => p.id !== except)
-  if (filtered.length > 0) return filtered[Math.floor(Math.random() * filtered.length)]
-  else return null
-}
-
 PreFooter.propTypes = {
-  except: PropTypes.string
+  cta: PropTypes.string,
+  quote: PropTypes.string,
+  url: PropTypes.string
 }
 
 export default PreFooter
