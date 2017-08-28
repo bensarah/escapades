6dbe53a938124c37138c90df27c2d30cff7537de
Sarah Mamy <mamysrh@gmail.com>
2017-08-28 22:33:01 +0200

    smartlook is backkkkkk

--- components/smartlook.js
+++ components/smartlook.js
@@ -9,12 +9,7 @@ class Smartlook extends Component {
     smartlookClient.disable()
   }
   render () {
-    return (
-      <div>
-        <button onClick={this.handleTag.bind(this)}>Tag</button>
-        <button onClick={this.handleDisable.bind(this)}>Disable</button>
-      </div>
-    )
+    return null
   }
   componentDidMount () {
     smartlookClient.init('09530f27a8e0b8bebdf57c13f7c27b74edb02ae4')
