30fe3606cecd405ec07cd4abab0faedc4598332f
Benjamin Tran Dinh <benjamin@mapbox.com>
2017-08-29 17:46:53 -0700

    Smooth scroll, try mountain colors?

--- components/footer.js
+++ components/footer.js
@@ -1,55 +1,68 @@
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Emoji from './emoji'
 import palette from '../styles/palette'
 import Link from 'next/link'
 import PreFooter from './pre-footer'
 
-const Footer = ({ cta, quote, url, prefooter }) => (
-  <div className='footer bottom w-full'>
-    {
-      prefooter
-      ? <PreFooter cta={cta} quote={quote} url={url}/>
-      : null
-    }
-    <div className='relative py30 px30 hmin120 azure'>
-      <div className='flex-parent flex-parent--column flex-parent--row-ml flew-parent--wrap'>
-        <a href='#' className='cursor-pointer pr18 pb6' onClick={() => window.scrollTo(window.pageYOffset, 0)}>
-          <Emoji name='arrow-up'/><span className='pl6'>Haut de page</span>
-        </a>
+class Footer extends Component {
+  render () {
+    return (
+      <div className='footer bottom w-full'>
+        {
+          this.props.prefooter
+          ? <PreFooter cta={this.props.cta} quote={this.props.quote} url={this.props.url}/>
+          : null
+        }
+        <div className='relative py30 px30 hmin120 azure'>
+          <div className='flex-parent flex-parent--column flex-parent--row-ml flew-parent--wrap'>
+            <a href='#' className='cursor-pointer pr18 pb6' onClick={() => window.scroll({
+              top: 0,
+              left: 0,
+              behavior: 'smooth'
+            })}>
+              <Emoji name='arrow-up'/><span className='pl6'>Haut de page</span>
+            </a>
 
-        <div className='pr18 pb6'>
-          <Link prefetch href='/'>
-            <a className='cursor-pointer'><Emoji name='house'/><span className='pl6'>Page d’accueil</span></a>
-          </Link>
+            <div className='pr18 pb6'>
+              <Link prefetch href='/'>
+                <a className='cursor-pointer'><Emoji name='house'/><span className='pl6'>Page d’accueil</span></a>
+              </Link>
+            </div>
+            <div className='pr18 pb6'>
+              <Link prefetch href='/about'>
+                <a className='cursor-pointer'><Emoji name='couple'/><span className='pl6'>À propos</span></a>
+              </Link>
+            </div>
+          </div>
+
+          <div className='bottom pt30 color-lighten25'>
+            <span>Copyright © {new Date().getFullYear()} Sarah & Benjamin</span><br/>
+            <span>Made with <Emoji name='heart'/> in San Francisco and Paris</span>
+          </div>
+          <img className='none block-ml absolute bottom right pr36 pt30 h120' src={`/static/deco/footer-vector.svg`} draggable={false}/>
         </div>
-        <div className='pr18 pb6'>
-          <Link prefetch href='/about'>
-            <a className='cursor-pointer'><Emoji name='couple'/><span className='pl6'>À propos</span></a>
-          </Link>
-        </div>
+        <style jsx>{`
+          .footer {
+            background-color: ${palette.bleuNuit};
+          }
+
+          .azure {
+            color: ${palette.azure};
+          }
+
+          .footer .clickable:hover, .footer a:hover {
+            color: ${palette.brightAzure};
+          }
+        `}</style>
       </div>
+    )
+  }
 
-      <div className='bottom pt30 color-lighten25'>
-        <span>Copyright © {new Date().getFullYear()} Sarah & Benjamin</span><br/>
-        <span>Made with <Emoji name='heart'/> in San Francisco and Paris</span>
-      </div>
-      <img className='none block-ml absolute bottom right pr36 pt30 h120' src={`/static/deco/footer-vector.svg`} draggable={false}/>
-    </div>
-    <style jsx>{`
-      .footer {
-        background-color: ${palette.bleuNuit};
-      }
-
-      .azure {
-        color: ${palette.azure};
-      }
-
-      .footer .clickable:hover, .footer a:hover {
-        color: ${palette.brightAzure};
-      }
-    `}</style>
-  </div>
-)
+  componentDidMount () {
+    require('smoothscroll-polyfill').polyfill()
+  }
+}
 
 Footer.propTypes = {
   prefooter: PropTypes.bool,
--- styles/palette.js
+++ styles/palette.js
@@ -1,11 +1,10 @@
 const palette = {
   brique: '#C63A22',
   tournesol: '#f0bd25',
-  taupe: '#776155',
   lavande: '#777D9B',
   bleuNuit: '#081926',
-  lightAzure: '#BCC5F4',
-  azure: '#777D9B',
+  lightAzure: '#35a2a5',
+  azure: '#19737c',
   grisClair: '#d9dade',
   lightenLavande: '#BCC5F4' // lavande is hsl(230,23,61) and lightenLavande is hsl(230,23,96)
 }
